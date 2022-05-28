29ebe68eb115a763abd23d73d4de45b5e66016a6
leomunizq <leonardo.mdq@hotmail.com>
2022-05-29 01:55:54 +0200

    import API.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React, { useState, useEffect } from 'react'
 import { CssBaseline, Grid } from '@material-ui/core'
 
+import { getPlacesData } from './api'
 import Header from './components/Header/Header'
 import List from './components/List/List'
 import Map from './components/Map/Map'
 import PlaceDetails from './components/PlaceDetails/PlaceDetails'
 
 const App = () => {
+  const [places, setPlaces] = useState([])
+
+  useEffect(() => {
+    getPlacesData().then(data => {
+      console.log(data)
+      setPlaces(data)
+    })
+  }, [])
+
   return (
     <>
       <CssBaseline />
