a998eb5e5400627cc6ced0df37b9fbd7fd4e4077
leomunizq <leonardo.mdq@hotmail.com>
2022-05-30 00:09:49 +0200

    insert pins and photo in the map.

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -8,7 +8,7 @@ import useStyles from './style'
 
 const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
   const classes = useStyles()
-  const isMobile = useMediaQuery('(min-width:600px)')
+  const isDesktop = useMediaQuery('(min-width:600px)')
 
   return (
     <div className={classes.mapContainer}>
@@ -25,7 +25,39 @@ const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
           console.log(e)
         }}
         onChildClick={''}
-      ></GoogleMapReact>
+      >
+        {places?.map((place, i) => (
+          <div
+            className={classes.markerContainer}
+            lat={Number(place.latitude)}
+            lng={Number(place.longitude)}
+            key={i}
+          >
+            {!isDesktop ? (
+              <LocationOnOutlinedIcon color="primary" fontSize="large" />
+            ) : (
+              <Paper elevation={3} className={classes.paper}>
+                <Typography
+                  className={classes.typography}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  {place.name}
+                </Typography>
+                <img
+                  className={classes.pointer}
+                  src={
+                    place.photo
+                      ? place.photo.images.large.url
+                      : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'
+                  }
+                  alt={place.name}
+                />
+              </Paper>
+            )}
+          </div>
+        ))}
+      </GoogleMapReact>
     </div>
   )
 }
