6102d0cb517f98f93bc539a0bf05cd868a428276
leomunizq <leonardo.mdq@hotmail.com>
2022-05-29 21:38:06 +0200

    insert lat and lng in the API

--- src/api/index.js
+++ src/api/index.js
@@ -2,20 +2,20 @@ import axios from 'axios'
 
 const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary'
 
-const options = {
-  params: {
-    bl_latitude: '11.847676',
-    tr_latitude: '12.838442',
-    bl_longitude: '109.095887',
-    tr_longitude: '109.149359'
-  },
-  headers: {
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
-    'X-RapidAPI-Key': 'edc02aaa9emsh185d154bcafc360p1522bcjsn65e24dc3ff3a'
-  }
-}
+// const options = {
+//   params: {
+//     bl_latitude: '11.847676',
+//     tr_latitude: '12.838442',
+//     bl_longitude: '109.095887',
+//     tr_longitude: '109.149359'
+//   },
+//   headers: {
+//     'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com',
+//     'X-RapidAPI-Key': 'edc02aaa9emsh185d154bcafc360p1522bcjsn65e24dc3ff3a'
+//   }
+// }
 
-export const getPlacesData = async (bounds, sw, ne) => {
+export const getPlacesData = async (sw, ne) => {
   try {
     const {
       data: { data }
