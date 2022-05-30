e7d682efacb1b2743504e3f40d7a46da7ecb7395
leomunizq <leonardo.mdq@hotmail.com>
2022-05-30 15:32:06 +0200

    change the name

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -5,13 +5,15 @@ import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
 import Rating from '@material-ui/lab/Rating'
 
 import useStyles from './style'
+import { mapStyles } from './mapStyles'
 
 const Map = ({
   setCoordinates,
   setBounds,
   coordinates,
   places,
-  setChildClicked
+  setChildClicked,
+  weatherData
 }) => {
   const classes = useStyles()
   const isDesktop = useMediaQuery('(min-width:600px)')
@@ -24,7 +26,11 @@ const Map = ({
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={''}
+        options={{
+          disableDefaultUI: true,
+          zoomControl: true,
+          styles: mapStyles
+        }}
         onChange={e => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng })
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
@@ -66,6 +72,15 @@ const Map = ({
             )}
           </div>
         ))}
+        {weatherData?.list?.length &&
+          weatherData.list.map((data, i) => (
+            <div key={i} lat={data.coord.lat} lng={data.coord.lng}>
+              <img
+                src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                height="70px"
+              />
+            </div>
+          ))}
       </GoogleMapReact>
     </div>
   )
